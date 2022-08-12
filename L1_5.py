be88f722df1cbe26d6df9aac5821a52cfc88710c
Grazyele Diniz <grazyele.diniz@gmail.com>
2022-08-12 16:43:10 -0300

    Algoritmo para calcular media ponderada

---
+++ L1_5.py
@@ -0,0 +1,10 @@
+import os
+os.system('cls')
+
+n1 = float(input('\nDigite a primeira nota: '))
+n2 = float(input('\nDigite a segunda nota: '))
+n3 = float(input('\nDigite a terceira nota: '))
+
+media = (n1*2 + n2*3 + n3*5)/10
+
+print('\nA média do aluno é: ', media)
